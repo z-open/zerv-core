bf6de082c8f27c91597e074e89fdc7de9c684f35
emmanuel telmon <etelmon@zimit.io>
2021-01-28 16:59:58 -0500

    3526 fixing issues with timezone in tests

--- test/cache.service.spec.js
+++ test/cache.service.spec.js
@@ -77,7 +77,7 @@ describe('cache.service', () => {
         let now;
 
         beforeEach(() => {
-            now = moment('Feb 6 2020 05:06:07', 'MMM DD YYYY hh:mm:ss').toDate();
+            now = moment('2020-02-06T10:06:07Z').toDate();
             jasmine.clock().install();
             jasmine.clock().mockDate(now);
             process.env.REDIS_ENABLED = false;
